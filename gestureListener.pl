b8f69dd10c711c5107df4ce160e8eb3aadfdd9d2
Kristopher Ruzic <krruzic@gmail.com>
2013-04-20 00:42:54 -0600

    Create gestureListener.pl
    First commit of script

---
+++ gestureListener.pl
@@ -0,0 +1,188 @@
+#!/usr/bin/perl -w
+# gestureListener.pl listens for pinch and swipe events
+use strict;
+use Time::HiRes();
+use X11::GUITest qw( :ALL );
+
+my @xHist = ();               # x coordinate history
+my @yHist = ();               # y coordinate history
+my @xHistThree = ();          # x coordinate history (three fingers)
+my @yHistThree = ();
+
+my $lastTime = 0;             # time monitor for TouchPad event reset
+my $eventTime = 0;            # ensure enough time has passed between events
+my $eventString = "default";  # the event to execute
+my $centerTouchPad = 3000;
+my $openSt         = 1000;    # start of open pinch
+my $openEn         = 500;     # end of open pinch
+my $closeSt        = 1000;    # start of close pinch
+my $closeEn        = 1000;    # end of close pinch
+
+my $synCmd = qq{synclient TouchpadOff=1 -m 10};
+my $currWind = GetInputFocus();
+die "couldn't get input window" unless $currWind;
+open(INFILE," $synCmd |") or die "can't read from synclient";
+
+while( my $line  = <INFILE>)
+{
+  chomp($line);
+  my(  $time, $x, $y, $z, $f ) = split " ", $line;
+  next if( $time =~ /time/ ); #ignore header lines
+  if( $time - $lastTime > 1 )
+  {
+    @xHist = ();
+    @yHist = ();
+    @xHistThree = ();
+    @yHistThree = ();
+  }#if time reset'''
+
+  $lastTime = $time;
+
+
+  # three finger swipe detection
+  if( $f eq "3" )
+  {
+    @xHist = ();
+    @yHist = ();
+
+    push @xHistThree, $x;
+    push @yHistThree, $y;
+    if( @xHistThree >= 2 && $xHistThree[0] ne $xHistThree[1])
+    {
+      print "@xHistThree\n";
+      my @srt = sort @xHistThree;
+      my @revSrt = reverse sort @xHistThree;
+
+      if( "@srt" eq "@xHistThree" )
+      {
+        # crtl + alt + right arrow - switch Right Workspace
+     print "Scrolling\n";
+        $eventString = "^(%({RIG}))";
+	   execEvent();
+      }
+      elsif( "@revSrt" eq "@xHistThree" )
+      {
+        # crtl + alt + left arrow - switch Left Workspace
+	   print "Scrolling\n";
+        $eventString = "^(%({LEF}))";
+	   execEvent();
+      }#if forward or backward
+
+      @xHistThree= ();
+    }#if more than 10 data points in 3 finger array
+
+    if( @yHistThree >= 2 && $yHistThree[0] ne $yHistThree[1])
+    {
+      print "@yHistThree\n";
+      my @srtY = sort @yHistThree;
+      my @revSrtY = reverse sort @yHistThree;
+
+      if( "@srtY" eq "@yHistThree" )
+      {
+        # crtl + alt + up - Workspace Overview
+        print "Overview\n";
+        $eventString = "^(%({UP}))";
+        execEvent();
+      }
+      elsif( "@revSrtY" eq "@yHistThree" )
+      {
+        # crtl + alt + down - Window Overview
+        print "Windows\n";
+        $eventString = "^(%({DWN}))";
+        execEvent();
+      }#if forward or backward
+    }
+  }
+  elsif( $f eq "2" || $f eq "1" )
+  {
+    # accept 1 or 2 finger entries as part of pinch section
+    @xHistThree = ();
+
+    push @xHist, $x;
+    push @yHist, $y;
+
+    if( @xHist > 50 )
+    {
+      if( (getStrAvg(\@xHist) > $closeSt && getStrAvg(\@yHist) > $closeSt) &&
+          (getEndAvg(\@yHist) < $closeEn && getEndAvg(\@yHist) < $closeEn) )
+      {
+        # wide to narrow detected, now search for enough 'wiggle'
+
+        my $tenX = 0;
+        my $tenY = 0;
+        for my $each( @xHist[40..49] ){ $tenX += $each }
+        for my $each( @yHist[40..49] ){ $tenY += $each }
+        $tenX = $tenX / 10;
+        $tenY = $tenY / 10;
+
+        my $diffX = 0;
+        my $diffY = 0;
+        for my $each( @xHist[40..49] ){ $diffX += abs( $each - $tenX ) }
+        for my $each( @yHist[40..49] ){ $diffY += abs( $each - $tenY ) }
+
+        # ctrl - decrease font size
+        if( ($diffX+$diffY) > 80 ){ $eventString = "^({-})" }
+
+        @xHist = ();
+        @yHist = ();
+        @xHistThree = ();
+
+      }#if x and y in range
+
+    }#if enough data for 50 close pinch detection
+
+    #open pinch requires substantially fewer data points
+    if( @xHist > 10 )
+    {
+      if( (getStrAvg(\@xHist) < $openSt && getStrAvg(\@yHist) < $openSt) &&
+          (getEndAvg(\@yHist) > $openEn && getEndAvg(\@yHist) > $openEn) )
+      {
+        # ctrl + increase font size
+        $eventString = "^({+})";
+
+        @xHist = ();
+        @yHist = ();
+        @xHistThree = ();
+      }#if absx and absy
+  else
+  {
+    # reset all data points, yes you can have 0 fingers at x,y
+    @xHist = ();
+    @yHist = ();
+    @xHistThree = ();
+  }# if not one or two or three fingers
+}#synclient line in
+sub execEvent
+{
+  # only process one event per time window
+  if( $eventString ne "default" )
+  {
+    print "Trying to exec\n";
+    if( abs(time - $eventTime) >= 1 )
+    {
+      $eventTime = time;
+      SendKeys("$eventString");
+    }#if enough time has passed
+    #$eventString = "default";
+  }#if non default event
+}
+close(INFILE);
+
+
+sub getStrAvg
+{
+  my $arrRef = $_[0];
+  my $val = (@$arrRef[0] + @$arrRef[1] + @$arrRef[2]) / 3;
+  $val = abs( $val - $centerTouchPad );
+  return($val);
+}#getStrAvg
+
+sub getEndAvg
+{
+  my $arrRef = $_[0];
+  my $val = (@$arrRef[@$arrRef-3] + @$arrRef[@$arrRef-2] +
+               @$arrRef[@$arrRef-1]) / 3;
+  $val = abs( $val - $centerTouchPad );
+  return($val);
+}#getEndAvg
+
